b5f0f12032aa2d66b397c0675de65e261cd3b17f
shantanu-sigmoid <98578520+shantanu-sigmoid@users.noreply.github.com>
2022-03-01 22:59:34 +0530

    setup

---
+++ temp.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+#establishing the connection
+conn = psycopg2.connect(
+   database="demo", user='postgres', password='password', host='localhost', port= '5432'
+)
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+#Executing an MYSQL function using the execute() method
+cursor.execute("select * from emp;")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+print(data)
+
+#Closing the connection
+conn.close()
+# Connection established to: (
+#    'PostgreSQL 11.5, compiled by Visual C++ build 1914, 64-bit',
+# )
